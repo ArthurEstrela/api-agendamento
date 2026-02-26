8613a7ceeb0ca6df1c852938bb9065020fb5a787
Arthur Estrela <arthurestrela369@gmail.com>
2026-02-26 18:39:19 -0300

    Ajustando

--- src/main/java/com/stylo/api_agendamento/adapters/outbound/persistence/service/ServiceMapper.java
+++ src/main/java/com/stylo/api_agendamento/adapters/outbound/persistence/service/ServiceMapper.java
@@ -7,12 +7,16 @@ import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface ServiceMapper {
-    
-    @Mapping(target = "serviceProviderId", source = "serviceProviderId")
-    @Mapping(target = "isActive", source = "active") // ✨ Entity (active) para Domain (isActive)
+
+    // De ServiceEntity para Service (Domínio)
+    // O campo na Entity chama-se 'isActive', o getter gerado é 'isActive()'.
+    // O MapStruct entende isso como a propriedade 'active'.
+    @Mapping(target = "isActive", source = "active")
     Service toDomain(ServiceEntity entity);
 
-    @Mapping(target = "serviceProviderId", source = "serviceProviderId")
-    @Mapping(target = "active", source = "isActive") // ✨ Domain (isActive) para Entity (active)
+    // De Service (Domínio) para ServiceEntity
+    // No seu Domínio, o campo é 'isActive', o getter é 'isActive()'.
+    // O MapStruct enxerga a propriedade como 'active'.
+    @Mapping(target = "isActive", source = "active")
     ServiceEntity toEntity(Service domain);
 }
